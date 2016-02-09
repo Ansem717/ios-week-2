c2e0cd6bedf9ad1168669411937376a70348aa7e
Andy Malik <me@andymalik.com>
2016-02-08 16:27:29 -0800

    Finished Twitter Clone and added detailed comments describing the code for review.

--- twitter-clone/TwitterClone/TwitterClone/HomeViewController.swift
+++ twitter-clone/TwitterClone/TwitterClone/HomeViewController.swift
@@ -8,34 +8,37 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource { //Creating new class named HomeViewController. It is inheriting from UIViewController and conforming to UITableViewDataSource.
     
-    var tweetData = [Tweet]() {
+    @IBOutlet weak var tableView: UITableView! // Connecting the view with the code
+    
+    var tweetData = [Tweet]() { //Array of Tweet classes based on the JSON files
         didSet {
-            //
+            self.tableView.reloadData() //This function reloads the table view each time something is changed on the array.
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidLoad() { // Original code
+        super.viewDidLoad() // ^^^^^^^^^^^^^^^^^
     }
     
     override func viewWillAppear(animated: Bool)
     {
-        super.viewWillAppear(animated)
+        super.viewWillAppear(animated)   //Informing the view to animate
     }
     
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning()     //// Original code
     }
     
     func setupTableView()
     {
-        self.tableView.tweetData = self
+        self.tableView.dataSource = self //Setting the datasource of the visual aid to the class HomeViewController
+                                        // This allows the view to update with any variable listed in this class.
     }
     
-    func update()
-    {
+    func update() // This function grabs the JSON, parses it, and if successful, it sets the tweetData in this class
+    {             // equal to an array of classes of Tweets (referenced in Tweet.swfit in Model)
         JSONParser.tweetJSONFrom(JSONParser.JSONData()) { (success, tweets) -> () in
             if success {
                 if let tweets = tweets {
@@ -48,19 +51,23 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController
 {
-    func configureCellForIndexPath(indexPath: NSIndexPath) -> UITableViewCell
+    func configureCellForIndexPath(indexPath: NSIndexPath) -> UITableViewCell //Function for conveyor-belting the table
     {
-        //MARK: Something is missing
+        let tweetCell = self.tableView.dequeueReusableCellWithIdentifier("tweetCell", forIndexPath: indexPath) //Grabs the tweetCell that has been dequeued by the user (dequeued means it is no longer visible)
+        let tweet = self.tweetData[indexPath.row] // Grabs the specific Tweet from the array tweetData
+        tweetCell.textLabel?.text = tweet.text // Sets the dequeued cell's text equal to the Tweet's text.
+        
+        return tweetCell //Returns the dequeued cell.
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int //NECESSARY TO CONFROM TO UITableViewDataSource
     {
-        self.tweetData.count
+        self.tweetData.count //Returns maximum number of rows at any given time.
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell //NECESSARY TO CONFORM TO UITableViewDataSource
     {
-        return self.configureCellForIndexPath(indexPath)
+        return self.configureCellForIndexPath(indexPath) //Returns the reused cell from confingureCellForIndexPath and adds it to the table
     }
 }
 
--- twitter-clone/TwitterClone/TwitterClone/JSONParser.swift
+++ twitter-clone/TwitterClone/TwitterClone/JSONParser.swift
@@ -8,33 +8,35 @@
 
 import Foundation
 
-typealias JSONParserCompletion = (success: Bool, tweets: [Tweet]?) -> ()
+typealias JSONParserCompletion = (success: Bool, tweets: [Tweet]?) -> () //Creates a type alias for a tuple, whose key-value pairs are boolean and an optional array of classes of Tweets and returns an initializer?
 
 class JSONParser
 {
     
-    class func tweetJSONFrom(data: NSData, completion: JSONParserCompletion)
+    class func tweetJSONFrom(data: NSData, completion: JSONParserCompletion) //Class function for Parsing the JSON
     {
-        do {
+        do { //ERROR HANDLING
             if let rootObject = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as? [[String : AnyObject]] {
-                
+                //RootObject is trying to serialize the JSON with an option that allows the dictionaries or arrays to be mutable, and checks if that is in the form of an array of dictionaries. If not, it goes to the catch down below.
             var tweets = [Tweet]()
-                
-                for tweetJSON in rootObject {
+            
+                for tweetJSON in rootObject { //FOR LOOP of the array of dictionaries. tweetJSON is each dictionary in the array.
                     if let
                         text = tweetJSON["text"] as? String,
                         id = tweetJSON["id_str"] as? String,
                         userJSON = tweetJSON["user"] as? [String : AnyObject]
-                    {
-                        let user = self.userFromTweetJSON(userJSON)
-                        let tweet = Tweet(text: text, id: id, user: user)
+                    {                                                         //The if let statement is checking if the key-value pairs are 
+                                                                             // returning the correct value type for the current tweet.
                         
-                        tweets.append(tweet)
+                        let user = self.userFromTweetJSON(userJSON)     // If all is true, pass the user dictionary into the Helper Function
+                        let tweet = Tweet(text: text, id: id, user: user) // Plug in all the data collected into a Tweet class.
+                        
+                        tweets.append(tweet) //Add that class to the array created earlier.
                     }
                 }
                 
-                completion(success: true, tweets: tweets)
-            }
+                completion(success: true, tweets: tweets) //This is a function because of the typealias.
+            }          //Above is when the JSON succeeds in grabbing data, below is when it fails.
         } catch _  {
             completion(success: false, tweets: nil)
         }
@@ -42,21 +44,21 @@ class JSONParser
 
     //MARK: Helper functions
 
-    class func userFromTweetJSON(tweetJSON: [String : AnyObject]) -> User
+    class func userFromTweetJSON(tweetJSON: [String : AnyObject]) -> User // HELPER FUNCTION, checks if user's data exists
     {
         guard let name = tweetJSON["name"] as? String else { fatalError("Failed to parse the name.") }
         guard let profileImgURL = tweetJSON["profile_image_url"] as? String else { fatalError() }
         guard let location = tweetJSON["location"] as? String else { fatalError() }
         
-        return User(name: name, profileImgURL: profileImgURL, location: location)
+        return User(name: name, profileImgURL: profileImgURL, location: location) // Sends back a User class with the correct data.
     }
 
     //MARK: Loading JSON from bundle
     class func JSONData() -> NSData
     {
-        guard let tweetJSONPath = NSBundle.mainBundle().URLForResource("tweet", withExtension: "json") else { fatalError("Local file tweet.json not found.") }
-        guard let tweetJSONData = NSData(contentsOfFile: tweetJSONPath) else { fatalError("Error creating NSData Object") }
-        return tweetJSONData
+        guard let tweetJSONPath = NSBundle.mainBundle().URLForResource("tweet", withExtension: "json") else { fatalError("Local file tweet.json not found.") } //IF Path is not found, fatal
+        guard let tweetJSONData = NSData(contentsOfURL: tweetJSONPath) else { fatalError("Error creating NSData Object") } //IF NSData cannot create the Object, fatal.
+        return tweetJSONData //NSData created the object successfully, return the whole thing.
     }
 }
 
